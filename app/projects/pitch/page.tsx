f8254d19e24712c5ea9c1c3d80a22eafe4bba753
zmar234 <zachmarino234@gmail.com>
2025-11-19 12:49:08 -0500

    Pitch copy edit

--- app/projects/pitch/page.tsx
+++ app/projects/pitch/page.tsx
@@ -4,8 +4,8 @@ export default function Pitch() {
             <div className="flex flex-col w-full gap-10 items-center">
 
                 <h2 className="self-start text-xl sm:text-2xl font-bold">introduction</h2>
-                <p>Pitch is a design kit that provides journalists with a more structured method for brainstorming story ideas. I created this card deck for my CAMD directed study to answer the question of what it means to think like a designer as a non-designer. I analyzed various design kits and synthesized them with user research surrounding the pitch-identifying habits of journalists, creating a deck of cards with different prompts, games, and ideas to spark story ideas.</p>
-
+                <p>Pitch is a design kit that provides journalists with a more structured method for brainstorming story ideas. I created this card deck for my College of Arts, Media, and Design (CAMD) directed study to answer the question of what it means to think like a designer as a non-designer. I analyzed various design kits and synthesized them with user research surrounding the pitch-identifying habits of journalists, creating a deck of cards with different prompts, games, and ideas to spark story ideas.</p>
+                
                 <h2 className="self-start text-xl sm:text-2xl font-bold">rationale</h2>
                 <p>I started this project the semester after my co-op at the Boston Globe. What started as an ultimately random co-op became a passion for designing around the experience of content. I wanted to do a directed study, and I decided to explore a way to combine my passion for journalism with my design skills. What I love about this project is that in creating it, I discovered a “6th sense” of sorts that journalists have for story ideas, and it reminded me a lot of the intuition I have as a designer. Answering the question of what it means to think like a designer, I believe we are all designers in our own way, just manifesting it differently.</p>
 
