09ee77abe91b0f0e4d1302bef82f48cf9a4feaec
zmar234 <zachmarino234@gmail.com>
2025-11-19 11:26:11 -0500

    Pitch content

--- app/projects/pitch/page.tsx
+++ app/projects/pitch/page.tsx
@@ -3,7 +3,24 @@ export default function Catalog8H() {
         <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-10 py-12 sm:py-24">
             <div className="flex flex-col w-full gap-10 items-center">
 
-                <h2 className="self-start text-xl sm:text-2xl font-bold">description to be added</h2>
+                <h2 className="self-start text-xl sm:text-2xl font-bold">introduction</h2>
+                <p>Pitch is a design kit that provides journalists with a more structured method for brainstorming story ideas. I created this card deck for my CAMD directed study to answer the question of what it means to think like a designer as a non-designer. I analyzed various design kits and synthesized them with user research surrounding the pitch-identifying habits of journalists, creating a deck of cards with different prompts, games, and ideas to spark story ideas.</p>
+
+                <h2 className="self-start text-xl sm:text-2xl font-bold">rationale</h2>
+                <p>I started this project the semester after my co-op at the Boston Globe. What started as an ultimately random co-op became a passion for designing around the experience of content. I wanted to do a directed study, and I decided to explore a way to combine my passion for journalism with my design skills. What I love about this project is that in creating it, I discovered a “6th sense” of sorts that journalists have for story ideas, and it reminded me a lot of the intuition I have as a designer. Answering the question of what it means to think like a designer, I believe we are all designers in our own way, just manifesting it differently.</p>
+
+                <h2 className="self-start text-xl sm:text-2xl font-bold">takeaways</h2>
+                <ol type="1" className="flex flex-col gap-2 self-start">
+                    <li>
+                        1. There&apos;s a balancing act between human-centered and activity-centered design, meaning that while our goal as designers is to make useful and meaningful experiences for users, we sometimes need to let users adapt to tools (rather than vice versa). I had to make my cards useful enough to tap into the “6th sense” journalists have, without acting as a crutch for it.
+                    </li>
+                    <li>
+                        2. The card modality, although somewhat basic, remains a compelling and engaging form of experience that people are inclined to interact with. Oftentimes, simple is better.
+                    </li>
+                    <li>
+                        3. Creating high-fidelity physical prototypes requires more preparation than digital ones, in my experience.
+                    </li>
+                </ol>
                 <h2 className="self-start text-xl sm:text-2xl font-bold">full documentation</h2>
                 <iframe className="max-w-3xl max-h-2xl w-full h-[650px]" src="https://embed.figma.com/slides/H1QxQ68WMjtA6wrn0egYWp/Pitch-Project-Presentation?node-id=1-587&embed-host=share"></iframe>
             </div>
