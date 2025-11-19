b6641f0ac4e484de295954a2d9e4dfaa36dc5fd2
zmar234 <zachmarino234@gmail.com>
2025-11-18 21:23:55 -0500

    Add resume to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,15 +3,19 @@ import Link from "next/link";
 const Header = () => {
     return (
         <header className="flex w-full max-w-3xl items-center justify-between">
-            <Link href={"/"} className="font-title text-4xl mb-5 mx-2 text-white">Zach Marino</Link>
+            <Link href={"/"} className="font-title text-2xl mb-3 mx-1 sm:text-4xl sm:mb-5 sm:mx-2 text-white">Zach Marino</Link>
             <nav>
                 <ul className="flex items-center gap-5">
                     <li>
                         <Link href={"/about"} className="font-body">about</Link>
                     </li>
                     <li>
-                        <Link href={"/contact"} className="font-body">contact</Link>
+                        <Link href={"/work"} className="font-body">work</Link>
                     </li>
+                    <li>
+                        <Link href={"Resume - Zach Marino - Nov 4.pdf"} target="_blank">resume</Link>
+                    </li>
+
                 </ul>
             </nav>
         </header>
