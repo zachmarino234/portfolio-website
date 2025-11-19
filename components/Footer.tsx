e43479ed2dc83d84e27d2a9f6a00cead0ad902fd
zmar234 <zachmarino234@gmail.com>
2025-11-19 02:35:06 -0500

    Footer copy fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="flex flex-col sm:flex-row w-full max-w-3xl sm:items-center justify-between gap-5">
             <hr className="sm:hidden"/>
             <div className="flex flex-col gap-2.5 text-sm sm:w-1/2">
-                <p>Website designed and developed by Zach Marino, inspired by Futuristic settings like <span className="italic">Mirror&apos;s Edge</span> and <span className="italic">Cyberpunk 2077</span></p>
+                <p>Website designed and developed by Zach Marino, inspired by futuristic settings like <span className="italic">Mirror&apos;s Edge</span> and <span className="italic">Cyberpunk 2077</span></p>
                 <p>No generative AI was used, besides the creation of the background animation</p>
             </div>
             <div className="flex gap-2.5">
