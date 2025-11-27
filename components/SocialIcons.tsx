8c4df2438b002630cc579d902decab01a349ba7d
zmar234 <zachmarino234@gmail.com>
2025-11-27 17:33:37 -0500

    Add aria labels to links

--- components/SocialIcons.tsx
+++ components/SocialIcons.tsx
@@ -5,16 +5,16 @@ import { LuMail } from "react-icons/lu";
 const SocialIcons = () => {
     return (
         <div className="flex gap-5">
-            <Link href={"mailto:hello@zmarino.com"}>
+            <Link href={"mailto:hello@zmarino.com"} aria-label="Email">
                 <LuMail className="w-5 h-5" />
             </Link>
-            <Link href={"https://www.linkedin.com/in/zach-marino/"} target="_blank">
+            <Link href={"https://www.linkedin.com/in/zach-marino/"} aria-label="Linkedin Link" target="_blank">
                 <FaLinkedin className="w-5 h-5" />
             </Link>
-            <Link href={"https://github.com/zachmarino234"} target="_blank">
+            <Link href={"https://github.com/zachmarino234"} aria-label="Github Link" target="_blank">
                 <FaGithub className="w-5 h-5" />
             </Link>
-            <Link href={"https://www.behance.net/zacharymarino"} target="_blank">
+            <Link href={"https://www.behance.net/zacharymarino"} aria-label="Behance Link" target="_blank">
                 <FaSquareBehance className="w-5 h-5" />
             </Link>
 
