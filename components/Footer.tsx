d73a3f8b92899856fcbe46b32f7b49f24ff2229f
zmar234 <zachmarino234@gmail.com>
2025-12-08 01:19:18 -0500

    Footer edit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <hr className="sm:hidden"/>
             <div className="flex flex-col gap-2.5 text-sm sm:w-1/2">
                 <p>Website designed and developed by Zach Marino, inspired by futuristic settings like <span className="italic">Mirror&apos;s Edge</span> and <span className="italic">Cyberpunk 2077</span></p>
-                <p>No generative AI was used, besides the creation of the background animation</p>
+                <p>No generative AI was used, besides the creation of the background animation and SEO</p>
             </div>
             <div className="flex gap-2.5">
                 <SocialIcons />
