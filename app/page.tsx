692f74c08e4652ab6a848a17b1d0faaf91efdff9
zmar234 <zachmarino234@gmail.com>
2026-07-09 10:51:16 -0400

    typo fix

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       </div>
       <div className="flex flex-col gap-2.5 px-10 sm:px-16  max-w-3xl">
         <p>Zach Marino is a product designer and web developer whose background in media and entertainment informs a design process that improves the relationship between creators and their tools, and between consumers and their goals. <Link href={"/about"} className="underline" aria-label="Read More">read more</Link></p>
-        <p>Prev. Product Design Internship at Dow Jones and Product Management Co-op at the Boston Globe. Currently job hunting and doing freelance dessign work on Broadway!</p>
+        <p>Prev. Product Design Internship at Dow Jones and Product Management Co-op at the Boston Globe. Currently job hunting and doing freelance design work on Broadway!</p>
         <div className="flex w-full items-center gap-5">
           <p className="py-3">Find me here:</p>
           <SocialIcons />
